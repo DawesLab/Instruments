8b455b39cf2fdf3e91998ccbb553eb1ec75cad93
Andrew M. C. Dawes <dawes@pacificu.edu>
2013-08-02 00:02:31 -0700

    adding command line options for tek scopes

--- TekScope.py
+++ TekScope.py
@@ -4,43 +4,84 @@ import matplotlib.pyplot as plot
  
 import instrument
  
-""" Example program to plot the Y-T data from Channel 1"""
+""" Program to plot the Y-T data from selected channels."""
  
+# Add command line options:
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_optiln("-1", "--channel1", dest="ch1",
+                  help="Get data from channel 1", default=False, action="store_true")
+parser.add_optiln("-2", "--channel2", dest="ch2",
+                  help="Get data from channel 2", default=False, action="store_true")
+parser.add_optiln("-r1", "--ref1", dest="ref1",
+                  help="Get data from ref 1", default=False, action="store_true")
+parser.add_optiln("-r2", "--ref2", dest="ref2",
+                  help="Get data from ref 2", default=False, action="store_true")
+
+(options, args) = parser.parse_args()
+
+def read_data():
+	""" Function for reading data and parsing binary into numpy array """
+	test.write("CURV?")
+	rawdata = test.read(9000)
+
+	# First few bytes are characters to specify the length of the transmission. Need to strip these off:
+	# we'll assume a 5000-byte transmission so the string would be "#45000" and we therefor strip 6 bytes.
+	return numpy.frombuffer(rawdata[6:-1], 'i2')
+	
+
 # Initialize our scope
 test = instrument.TekScope1000("/dev/usbtmc0")
  
 # Stop data acquisition
 test.write("ACQ:STATE STOP")
  
-# Grab the data from channel 1
-test.write("DATA:SOURCE CH1")
+# Set data width to 2 for better resolution
+test.write("DATA:WIDTH 2")
 
-# Set data format to ASCII
-test.write("DATA:ENCD ASCII")
+# Set data format to binary, zero is center-frame and big endian
+test.write("DATA:ENCD SRI")
+
+#TODO: replace with automatic selection using command:
+# SELECT? returns SELECT:CH1 0;CH2 1;MATH 1;REFA 0;REFB 1
+if ch1:
+	# Grab the data from channel 1
+	test.write("DATA:SOURCE CH1")
+	ch1data = read_data()
  
-test.write("CURV?")
-rawdata = test.read(9000)
-data = numpy.frombuffer(rawdata, 'B')
- 
+if ch2:
+	test.write("DATA:SOURCE CH2")
+	ch2data = read_data() 
+
+if ref1:
+	test.write("DATA:SOURCE REF1")
+	ref1data = read_data()
+
+if ref2:
+	test.write("DATA:SOURCE REF2")
+	ref2data = read_data()
+
 # Get the voltage scale
 test.write("CH1:SCALE?")
-voltscale = float(test.read(20))
+voltscale1 = float(test.read(20))
  
 # And the voltage offset
 test.write("CH1:POSITION?")
-voltoffset = float(test.read(20))
+voltoffset1 = float(test.read(20))
 
+ch1data = ((ch1data/65535.0 * 10) - voltoffset1) * voltscale1
 
-#TODO this is all leftover, and probably not valid!
+# Get the voltage scale
+test.write("CH2:SCALE?")
+voltscale2 = float(test.read(20))
  
-# Walk through the data, and map it to actual voltages
-# First invert the data (ya rly)
-data = data * -1 + 255
- 
-# Now, we know from experimentation that the scope display range is actually
-# 30-229.  So shift by 130 - the voltage offset in counts, then scale to
-# get the actual voltage.
-data = (data - 130.0 - voltoffset/voltscale*25) / 25 * voltscale
+# And the voltage offset
+test.write("CH2:POSITION?")
+voltoffset2 = float(test.read(20))
+
+ch2data = ((ch2data/65535.0 * 10) - voltoffset2) * voltscale2
+
  
 # Get the timescale
 test.write("HORIZONTAL:MAIN:SCALE?")
@@ -49,33 +90,28 @@ timescale = float(test.read(20))
 # Get the timescale offset
 test.write("HORIZONTAL:MAIN:POSITION?")
 timeoffset = float(test.read(20))
+
+# Get the length of the horizontal record
+test.write("HORIZONTAL:RECORDLENGTH?")
+time_size = int(test.read(30))
  
 # Now, generate a time axis.  The scope display range is 0-600, with 300 being
 # time zero.
-time = numpy.arange(-300.0/50*timescale, 300.0/50*timescale, timescale/50.0)
- 
-# If we generated too many points due to overflow, crop the length of time.
-if (time.size > data.size):
-    time = time[0:600:1]
- 
-# See if we should use a different time axis
-if (time[599] < 1e-3):
-    time = time * 1e6
-    tUnit = "uS"
-elif (time[599] < 1):
-    time = time * 1e3
-    tUnit = "mS"
-else:
-    tUnit = "S"
+#time = numpy.arange(-300.0/50*timescale, 300.0/50*timescale, timescale/50.0)
+time = numpy.arange(time_size)
+
+time = time / time_size * timescale * 10
+
  
 # Start data acquisition again, and put the scope back in local mode
 test.write("ACQ:STATE RUN")
 test.write("UNLOCK ALL")
- 
+
 # Plot the data
-plot.plot(time, data)
-plot.title("Oscilloscope Channel 1")
+plot.plot(time,ch1data)
+plot.plot(time,ch2data)
+plot.title("Oscilloscope Channel 1&2")
 plot.ylabel("Voltage (V)")
-plot.xlabel("Time (" + tUnit + ")")
-plot.xlim(time[0], time[599])
+plot.xlabel("Time (S)")
+#plot.xlim(time[0], time[599]
 plot.show()
