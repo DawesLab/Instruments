137ef2157f3ba5a11f7e19fb3614efe33df7179f
Noah Holte <noahtholte@gmail.com>
2014-04-05 23:07:48 -0700

    first version with all features working

--- TekScopeGUI.py
+++ TekScopeGUI.py
@@ -12,7 +12,7 @@ import random
 import wx
 
 # The recommended way to use wx with mpl is with the WXAgg
-# backend. 
+# backend.
 #
 import matplotlib
 matplotlib.use('WXAgg')
@@ -21,63 +21,67 @@ from matplotlib.backends.backend_wxagg import \
     FigureCanvasWxAgg as FigCanvas, \
     NavigationToolbar2WxAgg as NavigationToolbar
 
+import instrument
+import numpy
+
 
 class ScopeFrame(wx.Frame):
     """ The main frame of the application
     """
     title = 'Tektronix scope download'
-    
+    inst = instrument.TekScope1000("/dev/usbtmc0")
+
     def __init__(self):
         wx.Frame.__init__(self, None, -1, self.title)
-        
+
         self.data = [0, 0, 0, 0, 0]
-        
+
         self.create_menu()
         self.create_status_bar()
         self.create_main_panel()
-        
+
         self.draw_figure()
 
     def create_menu(self):
         self.menubar = wx.MenuBar()
-        
+
         menu_file = wx.Menu()
         m_expt = menu_file.Append(-1, "&Save data\tCtrl-S", "Save data to file")
         self.Bind(wx.EVT_MENU, self.on_save_data, m_expt)
         menu_file.AppendSeparator()
         m_exit = menu_file.Append(-1, "E&xit\tCtrl-X", "Exit")
         self.Bind(wx.EVT_MENU, self.on_exit, m_exit)
-        
+
         menu_help = wx.Menu()
         m_about = menu_help.Append(-1, "&About\tF1", "About this software")
         self.Bind(wx.EVT_MENU, self.on_about, m_about)
-        
+
         self.menubar.Append(menu_file, "&File")
         self.menubar.Append(menu_help, "&Help")
         self.SetMenuBar(self.menubar)
 
     def create_main_panel(self):
         """ Creates the main panel with all the controls on it:
-             * mpl canvas 
+             * mpl canvas
              * mpl navigation toolbar
              * Control panel for interaction
         """
         self.panel = wx.Panel(self)
-        
-        # Create the mpl Figure and FigCanvas objects. 
+
+        # Create the mpl Figure and FigCanvas objects.
         # 5x4 inches, 100 dots-per-inch
         #
         self.dpi = 100
         self.fig = Figure((5.0, 4.0), dpi=self.dpi)
         self.canvas = FigCanvas(self.panel, -1, self.fig)
-        
-        # Since we have only one plot, we can use add_axes 
+
+        # Since we have only one plot, we can use add_axes
         # instead of add_subplot, but then the subplot
         # configuration tool in the navigation toolbar wouldn't
         # work.
         #
         self.axes = self.fig.add_subplot(111)
-        
+
         self.ch1button = wx.Button(self.panel, -1, "CH1")
         self.Bind(wx.EVT_BUTTON, self.on_ch1_button, self.ch1button)
 
@@ -90,7 +94,7 @@ class ScopeFrame(wx.Frame):
         self.refbbutton = wx.Button(self.panel, -1, "RefB")
         self.Bind(wx.EVT_BUTTON, self.on_refb_button, self.refbbutton)
 
-        self.cb_grid = wx.CheckBox(self.panel, -1, 
+        self.cb_grid = wx.CheckBox(self.panel, -1,
             "Show Grid",
             style=wx.ALIGN_RIGHT)
         self.Bind(wx.EVT_CHECKBOX, self.on_cb_grid, self.cb_grid)
@@ -98,16 +102,16 @@ class ScopeFrame(wx.Frame):
         # Create the navigation toolbar, tied to the canvas
         #
         self.toolbar = NavigationToolbar(self.canvas)
-        
+
         #
         # Layout with box sizers
         #
-        
+
         self.vbox = wx.BoxSizer(wx.VERTICAL)
         self.vbox.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
         self.vbox.Add(self.toolbar, 0, wx.EXPAND)
         self.vbox.AddSpacer(10)
-        
+
         self.hbox = wx.BoxSizer(wx.HORIZONTAL)
         flags = wx.ALIGN_LEFT | wx.ALL | wx.ALIGN_CENTER_VERTICAL
         self.hbox.Add(self.ch1button, 0, border=3, flag=flags)
@@ -116,12 +120,12 @@ class ScopeFrame(wx.Frame):
         self.hbox.Add(self.refbbutton, 0, border=3, flag=flags)
         self.hbox.Add(self.cb_grid, 0, border=3, flag=flags)
         self.hbox.AddSpacer(30)
-        
+
         self.vbox.Add(self.hbox, 0, flag = wx.ALIGN_LEFT | wx.TOP)
-        
+
         self.panel.SetSizer(self.vbox)
         self.vbox.Fit(self)
-    
+
     def create_status_bar(self):
         self.statusbar = self.CreateStatusBar()
 
@@ -133,80 +137,82 @@ class ScopeFrame(wx.Frame):
 
         # clear the axes and redraw the plot anew
         #
-        self.axes.clear()        
+        self.axes.clear()
         self.axes.grid(self.cb_grid.IsChecked())
-        
-        self.axes.plot(self.data) 
+
+        self.axes.plot(self.data)
         self.canvas.draw()
-    
+
     def on_cb_grid(self, event):
         self.draw_figure()
-    
+
     def on_ch1_button(self, event):
-	self.data = [1, 2, 1, 2]
+        self.data = inst.get_data("CH1")
         self.draw_figure()
-    
+
     def on_ch2_button(self, event):
-	self.data = [2, 2, 1, 2]
+        self.data = inst.get_data("CH2")
         self.draw_figure()
-    
+
     def on_refa_button(self, event):
-	self.data = [3, 2, 1, 2]
+        self.data = inst.get_data("REFA")
         self.draw_figure()
-    
+
     def on_refb_button(self, event):
-	self.data = [4, 2, 1, 2]
-        self.draw_figure()
-    
-    def on_text_enter(self, event):
+        self.data = inst.get_data("REFB")
         self.draw_figure()
 
     def on_save_data(self, event):
-        file_choices = "PNG (*.png)|*.png"
-        
+        file_choices = "Numpy (*.npy)|*.npy"
+
         dlg = wx.FileDialog(
-            self, 
-            message="Save plot as...",
+            self,
+            message="Save data as...",
             defaultDir=os.getcwd(),
-            defaultFile="plot.png",
+            defaultFile="data",
             wildcard=file_choices,
             style=wx.SAVE)
-        
+
         if dlg.ShowModal() == wx.ID_OK:
             path = dlg.GetPath()
-            self.canvas.print_figure(path, dpi=self.dpi)
-            self.flash_status_message("Saved to %s" % path)
-        
+            #self.canvas.print_figure(path, dpi=self.dpi)
+            numpy.save(path, self.data)
+            self.flash_status_message("Data saved to %s" % path)
+
     def on_exit(self, event):
         self.Destroy()
-        
+
     def on_about(self, event):
         msg = """ View and save data from Tektronix scope:
-        
+
          * Use the matplotlib navigation bar
-	 * Select a channel to view or save
+         * Select a channel to view or save
          * Save the data to a file using the File menu
         """
         dlg = wx.MessageDialog(self, msg, "About", wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
-    
+
     def flash_status_message(self, msg, flash_len_ms=1500):
         self.statusbar.SetStatusText(msg)
         self.timeroff = wx.Timer(self)
         self.Bind(
-            wx.EVT_TIMER, 
-            self.on_flash_status_off, 
+            wx.EVT_TIMER,
+            self.on_flash_status_off,
             self.timeroff)
         self.timeroff.Start(flash_len_ms, oneShot=True)
-    
+
     def on_flash_status_off(self, event):
         self.statusbar.SetStatusText('')
 
 
 if __name__ == '__main__':
+    inst = instrument.TekScope1000("/dev/usbtmc0")
+
     app = wx.PySimpleApp()
     app.frame = ScopeFrame()
     app.frame.Show()
+
+
     app.MainLoop()
 
